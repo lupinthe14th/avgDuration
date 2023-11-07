a198308a3a6d4c953507ccc2d7288eeb37642e21
Hideo Suzuki <2602639+lupinthe14th@users.noreply.github.com>
2023-11-07 22:33:17 +0900

    feat: added min and max duration (#7)
    * feat: added min and max duration
    
    * fix: exit with error code on error
    
    * fix: use log.Fatalf instead of fmt.Println and os.Exit
    
    * chore: add bump.yml

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
+	"math"
 	"os"
 	"time"
 )
@@ -18,27 +20,51 @@ func main() {
 	// Read from standard input
 	inputData, err := io.ReadAll(os.Stdin)
 	if err != nil {
-		fmt.Println("Error reading from stdin:", err)
-		return
+		log.Fatalf("Error reading from stdin: %s", err)
 	}
 
 	var tasks [][]Task
 	err = json.Unmarshal([]byte(inputData), &tasks)
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		log.Fatalf("Error: %s", err)
 	}
 
-	var totalDuration time.Duration
+	var (
+		totalDuration time.Duration
+		minDuration   = time.Duration(math.MaxInt64)
+		maxDuration   time.Duration
+		taskCount     int
+	)
+
 	for _, taskGroup := range tasks {
 		for _, task := range taskGroup {
-			started, _ := time.Parse(time.RFC3339Nano, task.StartedAt)
-			created, _ := time.Parse(time.RFC3339Nano, task.CreatedAt)
+			started, err := time.Parse(time.RFC3339Nano, task.StartedAt)
+			if err != nil {
+				log.Fatalf("Error parsing dates for StartedAt: %s", err)
+			}
+			created, err := time.Parse(time.RFC3339Nano, task.CreatedAt)
+			if err != nil {
+				log.Fatalf("Error parsing dates for CreatedAt: %s", err)
+			}
 			duration := started.Sub(created)
+
+			if duration < minDuration {
+				minDuration = duration
+			}
+			if duration > maxDuration {
+				maxDuration = duration
+			}
 			totalDuration += duration
+			taskCount++
 		}
 	}
 
-	averageDuration := totalDuration / time.Duration(len(tasks)*len(tasks[0]))
+	if taskCount == 0 {
+		log.Fatalln("No tasks to process.")
+	}
+
+	averageDuration := totalDuration / time.Duration(taskCount)
 	fmt.Println("Average duration:", averageDuration)
+	fmt.Println("Minimum duration:", minDuration)
+	fmt.Println("Maximum duration:", maxDuration)
 }
